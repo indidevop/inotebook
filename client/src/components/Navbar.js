aae035e12167dab1a293aaf6016c656bf4d6cb46
Amey Sawalkar <ameykishor@gmail.com>
2023-06-21 19:14:32 +0530

    ready for deployment

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">iNotebook</Link>
+        <Link className="navbar-brand" to="/">Cloud Notebook</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
